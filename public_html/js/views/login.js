e0053a914595c8963f5611a40bcd2cf4c7b11b48
AlexandriusTheFirst <smirnova.a.yu@gmail.com>
2015-09-28 16:52:28 +0300

    json objects

--- public_html/js/views/login.js
+++ public_html/js/views/login.js
@@ -13,7 +13,27 @@ define([
             // TODO
         },
         render: function () {
+						
             $(this.el).html(this.template());
+						
+						$("#idForm").on("submit", function(event) {
+								var url = "/signin";
+								event.preventDefautlt();
+
+								$.ajax({
+											 type: "POST",
+											 url: url,
+											 data: $(this).serialize(),
+											 
+											 success: function(data)
+											 {
+													 window.location.replace("/#main");
+												   //postDispatcher(data);
+											 }
+										 });
+
+						});
+																			 
             return this;
         },
         show: function () {
--- public_html/js/views/main.js
+++ public_html/js/views/main.js
@@ -14,6 +14,8 @@ define([
         },
         render: function () {
             $(this.el).html(this.template());
+						
+
             return this;
         },
         show: function () {
