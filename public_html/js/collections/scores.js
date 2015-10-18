3d0fe4d52994bacba5cd273d2339b197a8466d72
AlexandriusTheFirst <smirnova.a.yu@gmail.com>
2015-10-18 23:15:35 +0300

    scoreboard fixed

--- public_html/js/collections/scores.js
+++ public_html/js/collections/scores.js
@@ -3,32 +3,33 @@ define([
 		'models/score'
 ], function(
     Backbone,
-		PlayerModel
+    PlayerModel
 ){
 		
-		var	players = [
-			new PlayerModel({name:'Jane', score: 1240}),
-			new PlayerModel({name:'Jhon', score: 350}),
-			new PlayerModel({name:'Jim', score: 3400}),
-			new PlayerModel({name:'Nonna', score: 3240}),
-			new PlayerModel({name:'Max', score: 1340}),
-			new PlayerModel({name:'Rita', score: 2240}),
-			new PlayerModel({name:'Tom', score: 3420}),
-			new PlayerModel({name:'Linda', score: 1420}),
-			new PlayerModel({name:'Ben', score: 2620}),
-			new PlayerModel({name:'David', score: 3410})
-		];
+    var	players = [
+	new PlayerModel({name:'Jane', score: 1240}),
+	new PlayerModel({name:'Jhon', score: 350}),
+	new PlayerModel({name:'Jim', score: 3400}),
+	new PlayerModel({name:'Nonna', score: 3240}),
+	new PlayerModel({name:'Max', score: 1340}),
+	new PlayerModel({name:'Rita', score: 2240}),
+	new PlayerModel({name:'Tom', score: 3420}),
+	new PlayerModel({name:'Linda', score: 1420}),
+	new PlayerModel({name:'Ben', score: 2620}),
+	new PlayerModel({name:'David', score: 3410})
+    ];
 
     var PlayerCollection = Backbone.Collection.extend({
-			model: PlayerModel,
+	model: PlayerModel,
+	url: '/scoreboard',
 
-			comparator: function (playerA, playerB) {
-				var scoreDiff = playerB.get('score') - playerA.get('score');
-				if (scoreDiff === 0) {
-					 return playerA.get('name') < playerB.get('name') ? -1 : 1;
-				}
-				return scoreDiff;
-			}
+	comparator: function (playerA, playerB) {
+	    var scoreDiff = playerB.get('score') - playerA.get('score');
+	    if (scoreDiff === 0) {
+		return playerA.get('name') < playerB.get('name') ? -1 : 1;
+	    }
+	    return scoreDiff;
+	}
     });
 
     return new PlayerCollection(players);
