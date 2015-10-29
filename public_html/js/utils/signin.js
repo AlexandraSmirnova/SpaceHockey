4753ff0c8504aac63c0b19aea83e2ffe03cadd0e
AlexandriusTheFirst <smirnova.a.yu@gmail.com>
2015-10-29 20:23:05 +0300

    mechanic

--- public_html/js/utils/signin.js
+++ public_html/js/utils/signin.js
@@ -7,37 +7,37 @@ function(
 
     var SigninManager = function(){
 	
-	this.signinRequest = function(model){
-	    console.log("request");
-	    var formData = {
-		'login':    $(form_class + " input[name = login]").val(),
-		'password': $(form_class +" input[name = password]").val() 
-	    };
+		this.signinRequest = function(model){
+		    console.log("request");
+		    var formData = {
+				'login':    $(form_class + " input[name = login]").val(),
+				'password': $(form_class +" input[name = password]").val() 
+		    };
 
-	    $.ajax({
-		type: "POST",
-		url: "/auth/signin",		   
-		data: JSON.stringify(formData),
+		    $.ajax({
+				type: "POST",
+				url: "/auth/signin",		   
+				data: JSON.stringify(formData),
 
-		success: function(data){
-		    alert(data);
-		    data =  JSON.parse(data);
+				success: function(data){
+				    alert(data);
+				    data =  JSON.parse(data);
 
-		    if(parseInt(data["status"], 10) == "200"){ 
-			localStorage.clear();
-			localStorage.setItem( 'user', JSON.stringify(data) );
-			model.set(formData);
-			model.save();			   
-			Backbone.history.navigate('game', {trigger: true});
-		    }
-		    else{
-			var $error = $(".form__row_errors"); 
-			$error.append("Login or password is incorrect!");
-			$error.show();
-		    }
+				    if(parseInt(data["status"], 10) == "200"){ 
+						localStorage.clear();
+						localStorage.setItem( 'user', JSON.stringify(data) );
+						model.set(formData);
+						model.save();			   
+						Backbone.history.navigate('game', {trigger: true});
+				    }
+				    else{
+						var $error = $(".form__row_errors"); 
+						$error.append("Login or password is incorrect!");
+						$error.show();
+				    }
+				}
+		    });
 		}
-	    });
-	}
     }
 	
 
--- public_html/js/views/main.js
+++ public_html/js/views/main.js
@@ -13,41 +13,39 @@ define([
 
     var Main = Backbone.View.extend({
         template: tmpl,
-	user: User,
-	
-	events: {
-	    "click .menu__item_logout": "logout"
-	},
-	
-	initialize: function () {
+    	user: User,
+    	
+    	events: {
+    	    "click .menu__item_logout": "logout"
+    	},
+    	
+    	initialize: function () {
             $('.page').append(this.el);
             this.render();
-	    that = this;
-	    this.listenTo(this.user, 'change', function(){ alert("Model changed!"); that.render();} );	   
-	    	   
+    	    that = this;
+    	    this.listenTo(this.user, 'change', function(){ alert("Model changed!"); that.render();} );	   
+    	    	   
         },
 
-	logout: function(){
-	    SignoutManager.exitRequest(this.user);
-	    this.render();
-	},
-	        
-     	render: function () {
+    	logout: function(){
+    	    SignoutManager.exitRequest(this.user);
+    	    this.render();
+    	},
+    	        
+        render: function () {
             this.$el.html(this.template(JSON.parse(localStorage.getItem("user"))));
         },
 
         show: function () {
-	    this.trigger('show', this);
-	    this.$el.show();
+    	    this.trigger('show', this);
+    	    this.$el.show();
         },
 
         hide: function () {
             this.$el.hide();
         },
-	
+    	
     });
 
-
-    var main = new Main();
-    return main;
+    return new Main();
 });
